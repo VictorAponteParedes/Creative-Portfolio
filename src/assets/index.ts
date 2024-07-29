dac699cbeb993ed6861de5bf4682e6d89ab76f85
Victor Aponte <victor.aponte@azeta.com.py>
2024-07-29 02:39:19 -0300

    Agrega: sesion de proyectos personales

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,7 +2,9 @@
 import certifiReactNative from './image/certificado-react-native.jpg';
 import certifiDjandoTest from './image/cerificador-django-testes.png';
 import perfilImagen from './image/perfil-imagen.jpeg'
+import cerkImagen from './image/imagen-cerk.png'
+import pipImagen from './image/imagen-pip.png'
+import tnaImagen from './image/imagen-na.png'
+import abcImagen from './image/imagen-abc.png'
 
-
-
-export { certifiReactNative, certifiDjandoTest, perfilImagen };
+export { certifiReactNative, certifiDjandoTest, perfilImagen, cerkImagen, pipImagen, tnaImagen, abcImagen };
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,6 @@
 // HomeScreen.tsx
 import React from 'react';
+import { cerkImagen, tnaImagen, pipImagen, abcImagen } from '../../assets';
 
 export const HomeScreen: React.FC = () => {
     return (
@@ -17,10 +18,28 @@ export const HomeScreen: React.FC = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {/* Ejemplo de Tarjeta de Proyecto */}
                     <div className="bg-white shadow-md p-6 rounded-lg">
-                        <img src="[url-de-imagen]" alt="Proyecto 1" className="w-full h-32 object-cover mb-4 rounded-md" />
-                        <h3 className="text-xl font-semibold mb-2">Nombre del Proyecto</h3>
-                        <p className="text-gray-600 mb-4">Descripción breve del proyecto.</p>
-                        <a href="[enlace-del-proyecto]" className="text-blue-500 hover:underline">Ver Más</a>
+                        <img src={tnaImagen} alt="Proyecto 1" className="w-full h-32 object-cover mb-4 rounded-md" />
+                        <h3 className="text-xl font-semibold mb-2">App Nueva Amaricana</h3>
+                        <p className="text-gray-600 mb-4">Aplicación de la tarjeta de crédito de Nueva Americana</p>
+                        <a href="#" className="text-blue-500 hover:underline">Ver Más</a>
+                    </div>
+                    <div className="bg-white shadow-md p-6 rounded-lg">
+                        <img src={pipImagen} alt="Proyecto 1" className="w-full h-32 object-cover mb-4 rounded-md" />
+                        <h3 className="text-xl font-semibold mb-2">App PIP</h3>
+                        <p className="text-gray-600 mb-4">PiP es tu nueva app para hacer compras con las mejores promociones..</p>
+                        <a href="#" className="text-blue-500 hover:underline">Ver Más</a>
+                    </div>
+                    <div className="bg-white shadow-md p-6 rounded-lg">
+                        <img src={cerkImagen} alt="Proyecto 1" className="w-full h-32 object-cover mb-4 rounded-md" />
+                        <h3 className="text-xl font-semibold mb-2">Cerk</h3>
+                        <p className="text-gray-600 mb-4">La ruta más rápida al éxito.</p>
+                        <a href="#" className="text-blue-500 hover:underline">Ver Más</a>
+                    </div>
+                    <div className="bg-white shadow-md p-6 rounded-lg">
+                        <img src={abcImagen} alt="Proyecto 1" className="w-full h-32 object-cover mb-4 rounded-md" />
+                        <h3 className="text-xl font-semibold mb-2">App ABC color</h3>
+                        <p className="text-gray-600 mb-4">Con la aplicación de ABC Color para Android Ud. podrá hojear a la edición impresa del diario tal como si fuera el papel.</p>
+                        <a href="#" className="text-blue-500 hover:underline">Ver Más</a>
                     </div>
                     {/* Más tarjetas de proyectos */}
                 </div>
@@ -29,7 +48,20 @@ export const HomeScreen: React.FC = () => {
             {/* Habilidades y Experiencia */}
             <section className="my-12">
                 <h2 className="text-3xl font-bold mb-6">Habilidades y Experiencia</h2>
-                {/* Aquí podrías incluir gráficos, listas o cualquier otro elemento visual */}
+                <div className="bg-white shadow-md p-6 rounded-lg">
+                    <h3 className="text-2xl font-semibold mb-2">Ide for Ideas (2021-2022)</h3>
+                    <p className="text-gray-600 mb-4">
+                        Participé en un equipo de desarrollo colaborativo, donde adquirí experiencia práctica en tecnologías como NodeJS y JavaScript. Colaboré en el desarrollo de un sistema de gestión de proyectos, contribuyendo al diseño y la implementación de nuevas funcionalidades. Trabajé estrechamente con colegas para resolver desafíos técnicos y optimizar el rendimiento del sistema. Aprendí sobre buenas prácticas de codificación, control de versiones y metodologías ágiles.
+                    </p>
+                    <h3 className="text-2xl font-semibold mb-2">Softec (2022-2024)</h3>
+                    <p className="text-gray-600 mb-4">
+                        Desarrollé aplicaciones móviles nativas utilizando React Native, optimizando así la experiencia del usuario en plataformas iOS y Android. Diseñé y desarrollé backend robustos utilizando Python y el framework Django, asegurando un rendimiento óptimo y una alta seguridad.
+                    </p>
+                    <h3 className="text-2xl font-semibold mb-2">Azeta (Actualidad)</h3>
+                    <p className="text-gray-600">
+                        Desarrollo de aplicaciones móviles.
+                    </p>
+                </div>
             </section>
 
             {/* Testimonios */}
